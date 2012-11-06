2b4d7f36ce820b6732b23b9b50c3dad8433f7bee
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2012-11-06 14:21:34 +0300

    Don't start uuid and sync at startup

--- console.sh
+++ console.sh
@@ -4,8 +4,9 @@ erl -pa ebin/ deps/*/ebin/ -eval "
 lists:foreach(fun application:start/1, [
 
 gproc, piqi, compiler, syntax_tools, lager,
-uuid, sync, billy_common, billy_session_c, billy_client
+billy_common, billy_session_c, billy_client
 
 ]),
-lager:set_loglevel(lager_console_backend, debug),
-sync:growl(false)."
+
+lager:set_loglevel(lager_console_backend, debug).
+"
