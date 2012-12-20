b479c9251a9cf1609ac363d17eb501777ebb5d1c
Dmitry Klionsky <d.klionsky@dev.powermemobile.com>
2012-12-20 12:06:29 +0300

    Add dializer and develop to makefile

--- develop.sh
+++ develop.sh
@@ -4,9 +4,10 @@ erl -pa ebin/ deps/*/ebin/ -eval "
 lists:foreach(fun application:start/1, [
 
 gproc, piqi, compiler, syntax_tools, lager,
-billy_common, billy_session_c, billy_client
+billy_common, billy_session_c, billy_client,
+sync
 
 ]),
 
-lager:set_loglevel(lager_console_backend, info).
+lager:set_loglevel(lager_console_backend, debug).
 "
