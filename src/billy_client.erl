71f7f1666981655bcf9e5cbd04c12a9e86cea21a
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-07-09 17:16:05 +0300

    Make reserve/5 to handle multiple service types at once

--- src/billy_client.erl
+++ src/billy_client.erl
@@ -3,8 +3,7 @@
 -export([
     start_session/4,
     stop_session/1,
-
-    reserve/6,
+    reserve/5,
     commit/1,
     rollback/1
 ]).
@@ -13,6 +12,10 @@
 
 -type billy_session_id() :: binary().
 -type billy_transaction_id() :: {SessionId::billy_session_id(), TransactionId::integer()}.
+
+-type service_type() :: binary().
+-type service_quantity() :: pos_integer().
+-type service_request() :: [{service_type(), service_quantity()}].
 
 %% ===================================================================
 %% API
@@ -37,26 +40,23 @@ stop_session(SessionId) ->
     ClientType::binary(),
     CustomerId::binary(),
     UserId::binary(),
-    ServiceType::binary(),
-    ServiceQuantity::pos_integer()
+    ServiceRequest::service_request()
 ) ->
     {accepted, TransactionId::billy_transaction_id()}
   | {rejected, Reason::any()}
   | {error, Reason::any()}.
-reserve(SessionId, ClientType, CustomerId, UserId, ServiceType, ServiceQuantity) when ServiceQuantity > 0 ->
+reserve(SessionId, ClientType, CustomerId, UserId, ServiceRequest)
+        when length(ServiceRequest) > 0 ->
     case billy_client_session:start_transaction(SessionId) of
         {ok, TransactionId} ->
-            {ok, EmptySvcContainer} = billy_service:cont_create(),
-            {ok, FullSvcContainer} = billy_service:cont_set_id(
-                EmptySvcContainer,
-                ServiceType,
-                #svc_details{quantity = ServiceQuantity}
-            ),
-            case billy_client_transaction:reserve(TransactionId, ClientType, CustomerId, UserId, FullSvcContainer) of
+            SvcCont = build_svc_cont(ServiceRequest),
+            case billy_client_transaction:reserve(TransactionId, ClientType,
+                    CustomerId, UserId, SvcCont) of
                 {ok, accepted} ->
                     {accepted, TransactionId};
                 {ok, {rejected, Reason}} ->
-                    % TODO: should not the transaction be stopped here? have it stopped already?
+                    %% TODO: should not the transaction be stopped here?
+                    %% have it stopped already?
                     {rejected, list_to_atom(binary_to_list(Reason))};
                 {error, Reason} ->
                     {error, Reason}
@@ -86,3 +86,18 @@ rollback(TransactionId) ->
         {error, Reason} ->
             {error, Reason}
     end.
+
+%% ===================================================================
+%% Internal
+%% ===================================================================
+
+build_svc_cont(SvcReq) ->
+    {ok, EmptySvcCont} = billy_service:cont_create(),
+    build_svc_cont(SvcReq, EmptySvcCont).
+
+build_svc_cont([], SvcCont) ->
+    SvcCont;
+build_svc_cont([{SvcType, SvcQnt} | SrvReq], SvcCont) ->
+    {ok, SvcCont2} = billy_service:cont_set_id(SvcCont,
+        SvcType, #svc_details{quantity = SvcQnt}),
+    build_svc_cont(SrvReq, SvcCont2).
