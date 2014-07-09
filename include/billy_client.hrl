d329a9aa808850e3a638cd526b5b2eb8d929f117
Dmitry Klionsky <dm.klionsky@gmail.com>
2014-07-09 12:12:23 +0300

    Refine constant

--- include/billy_client.hrl
+++ include/billy_client.hrl
@@ -1,11 +1,13 @@
 -ifndef(billy_client_hrl).
 -define(billy_client_hrl, included).
 
--define(CLIENT_TYPE_ONEAPI, <<"oneapi">>).
 -define(CLIENT_TYPE_FUNNEL, <<"funnel">>).
+-define(CLIENT_TYPE_MM,     <<"mm">>).
+-define(CLIENT_TYPE_ONEAPI, <<"oneapi">>).
+-define(CLIENT_TYPE_SOAP,   <<"soap">>).
 
--define(SERVICE_TYPE_SMS_ON, <<"sms_on">>).
+-define(SERVICE_TYPE_SMS_ON,  <<"sms_on">>).
 -define(SERVICE_TYPE_SMS_OFF, <<"sms_off">>).
--define(SERVICE_TYPE_SMS_INTERNATIONAL, <<"sms_int">>).
+-define(SERVICE_TYPE_SMS_INT, <<"sms_int">>).
 
 -endif.
